a8aabf20c8a03d77a6e7ae0cbc9080a5aa0eba53
Khonsa Mawadda <mawaddakhonsa25@gmail.com>
2022-10-24 11:00:21 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Khonsa Mawadda";
+$umur= 21;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaKakak = "Nabilla";
+$umurKakak = 23;
+
+echo "Nama kakak saya <strong>$namaKakak</strong>, kakak saya berusia $umurKakak tahun.<br>";
+
+$selisihUmur = $umurKakak - $umur;
+echo "Selisih umur saya dan kakak saya adalah $selisihUmur tahun."
+
+?>
